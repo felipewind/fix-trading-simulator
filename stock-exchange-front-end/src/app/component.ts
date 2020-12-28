dc6975e47aadb7a36be5fa7eeb05024bc1315711
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2020-12-27 21:02:56 -0300

    first version

--- stock-exchange-front-end/src/app/component.ts
+++ stock-exchange-front-end/src/app/component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
 import { Model } from "./repository.model";
+import { Order } from "./order.model";
+
 @Component({
     selector: "app",
     templateUrl: "template.html"
 })
 export class OrderComponent {
     model: Model = new Model();
+    getOrders(): Order[] {
+        return this.model.getOrders();
+    }
 }
