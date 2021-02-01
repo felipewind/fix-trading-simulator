3aa1b7a9a512fef305b9d2497138c9ee93b4aec7
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2021-02-01 20:02:15 -0300

    broker end first version

---
+++ broker-front-end/src/app/model/model.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { Model } from "./repository.model";
+import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
+import { RestDataSource , REST_URL } from "./rest.datasource";
+
+@NgModule({
+    imports: [HttpClientModule, HttpClientJsonpModule],
+    providers: [Model, RestDataSource,
+        { provide: REST_URL, useValue: `http://${location.hostname}:3500/products` }]
+})
+export class ModelModule { }
